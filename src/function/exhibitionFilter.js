b51878c29f1674fd075f45357f1a6b380384a0f3
yangunlee <yangun410@gmail.com>
2023-10-15 20:14:09 +0800

    Feat: change logic of currentFilter

--- src/function/exhibitionFilter.js
+++ src/function/exhibitionFilter.js
@@ -1,6 +1,8 @@
 export function currentFilter(arr) {
+  const _now =new Date()
+  const _currentDate= `${_now.getFullYear()}/${_now.getMonth()+1}/${_now.getDate()}`
   const currentExp = arr
-    .filter((exhibit) => new Date(exhibit.time.split("-")[1]) > new Date())
+    .filter((exhibit) => (new Date(exhibit.time.split("-")[1]) > new Date()||exhibit.time.split("-")[1]===_currentDate))
     .filter((exhibit) => new Date(exhibit.time.split("-")[0]) < new Date())
     .sort(
       (a, b) => new Date(a.time.split("-")[1]) - new Date(b.time.split("-")[1])
